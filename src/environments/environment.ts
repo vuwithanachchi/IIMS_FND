4fda3eed90def9927475c8335322247be2b8753d
vihangi withanachchi <64692535+vuwithanachchi@users.noreply.github.com>
2023-01-30 15:43:59 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   // tokenUrl: 'http://localhost:5000/api/v1/token',
   baseUrl: 'http://localhost:3000'
 };
